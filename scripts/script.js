43caf9ac2fc2f41052266a41df996451b95ac97e
Richard Olpindo <djricky088@yahoo.com>
2024-10-11 11:54:35 -1000

    Created type selection form, added type options through js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,25 @@
-import {typeEffectiveness} from "./typeEffectiveness.js";
+import { typeEffectiveness } from "./typeEffectiveness.js";
 
-Object.entries(typeEffectiveness).forEach((type) => {
-    const [pokemonType, attackType] = type;
-    if (pokemonType === "fire"){
-        console.log(attackType);
-    }
+const typeMatchups = Object.entries(typeEffectiveness);
+
+const types = []
+
+typeMatchups.forEach((type) => {
+  const [pokemonType, attackType] = type;
+  types.push(pokemonType)
+  // if (pokemonType === "fire"){
+  //     console.log(attackType);
+  // }
+});
+
+console.log(types);
+
+
+const pokemonDropdown = document.querySelectorAll(".pokemon-dropdown");
+
+console.log(pokemonDropdown);
+
+pokemonDropdown.forEach((selection)=>{
+    selection.innerHTML += types.map((type) =>
+    `<option value=${type}>${type}</option>`)
 })
-
