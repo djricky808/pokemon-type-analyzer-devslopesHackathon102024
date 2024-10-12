40fbf077b415126ae88a84326402f6a451f79a79
Richard Olpindo <djricky088@yahoo.com>
2024-10-12 13:38:49 -1000

    Modified type selection logic where the user could accidentally choose the first type to match the second, or if they removed the first typing the second typing would be reset

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,15 @@ const typeMatchups = Object.entries(typeEffectiveness);
 const types = [];
 
 typeMatchups.forEach((type) => {
-  const [pokemonType, attackType] = type;
+  const [pokemonType] = type;
   types.push(pokemonType);
-  // if (pokemonType === "fire"){
-  //     console.log(attackType);
-  // }
 });
 
 console.log(types);
 
 const pokemonDropdown1 = document.querySelector(".pokemon-dropdown1");
 const pokemonDropdown2 = document.querySelector(".pokemon-dropdown2");
+const pokemonForm = document.querySelector(".type-submission-form");
 
 pokemonDropdown1.innerHTML += types.map(
   (type) => `<option value=${type}>${type}</option>`
@@ -37,7 +35,16 @@ pokemonDropdown1.addEventListener("change", ()=> {selectedType1 = pokemonDropdow
   }
 })
 
-pokemonDropdown2.addEventListener("change", ()=> {selectedType2= pokemonDropdown2.value})
+pokemonDropdown2.addEventListener("change", ()=> {selectedType2= pokemonDropdown2.value;
+    for (let option of pokemonDropdown1.options) {
+      if (option.value === selectedType2) {
+        option.disabled = true;
+      } else {
+        option.disabled = false;
+      }
+    }
+}
+)
 
 function enableSecondTypeSelection() {
   pokemonDropdown2.disabled = false;
@@ -51,5 +58,14 @@ function enableSecondTypeSelection() {
 }
 
 function disableSecondTypeSelection() {
+  pokemonDropdown2.value = '';
   pokemonDropdown2.disabled = true;
+
 }
+
+pokemonForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  console.log(selectedType1);
+  console.log(selectedType2);
+  
+})
