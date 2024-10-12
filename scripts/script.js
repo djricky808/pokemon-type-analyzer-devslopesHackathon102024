1123f4f0d6bb3fb96384725f7ede075f19739663
Richard Olpindo <djricky088@yahoo.com>
2024-10-11 21:57:51 -1000

    added css to options, added booleans to options so that user must select a type before picking a second type. User also can't pick the same type as the first type

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ import { typeEffectiveness } from "./typeEffectiveness.js";
 
 const typeMatchups = Object.entries(typeEffectiveness);
 
-const types = []
+const types = [];
 
 typeMatchups.forEach((type) => {
   const [pokemonType, attackType] = type;
-  types.push(pokemonType)
+  types.push(pokemonType);
   // if (pokemonType === "fire"){
   //     console.log(attackType);
   // }
@@ -14,12 +14,40 @@ typeMatchups.forEach((type) => {
 
 console.log(types);
 
+const pokemonDropdown1 = document.querySelector(".pokemon-dropdown1");
+const pokemonDropdown2 = document.querySelector(".pokemon-dropdown2");
 
-const pokemonDropdown = document.querySelectorAll(".pokemon-dropdown");
+pokemonDropdown1.innerHTML += types.map(
+  (type) => `<option value=${type}>${type}</option>`
+);
 
-console.log(pokemonDropdown);
+pokemonDropdown2.innerHTML += types.map(
+  (type) => `<option value=${type}>${type}</option>`
+);
 
-pokemonDropdown.forEach((selection)=>{
-    selection.innerHTML += types.map((type) =>
-    `<option value=${type}>${type}</option>`)
+let selectedType1 = '';
+disableSecondTypeSelection();
+
+pokemonDropdown1.addEventListener("change", ()=> {selectedType1 = pokemonDropdown1.value;
+  console.log(selectedType1);
+  if (selectedType1 === ""){
+    disableSecondTypeSelection()
+  } else {
+  enableSecondTypeSelection();
+  }
 })
+
+function enableSecondTypeSelection() {
+  pokemonDropdown2.disabled = false;
+  for (let option of pokemonDropdown2.options) {
+    if (option.value === selectedType1){
+      option.disabled = true;
+    } else {
+      option.disabled = false;
+    }
+  }
+}
+
+function disableSecondTypeSelection() {
+  pokemonDropdown2.disabled = true;
+}
