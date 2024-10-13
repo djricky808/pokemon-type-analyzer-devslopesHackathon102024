456cb59ec2afcffeb1b32ad81b630ccf511aacc7
Richard Olpindo <djricky088@yahoo.com>
2024-10-12 19:33:15 -1000

    Added function that tiers each Pokemon type by effectiveness damage.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 import { typeEffectiveness } from "./typeEffectiveness.js";
 
 const typeMatchups = Object.entries(typeEffectiveness);
+console.log(typeEffectiveness);
 
 const types = [];
 
@@ -23,33 +24,34 @@ pokemonDropdown2.innerHTML += types.map(
   (type) => `<option value=${type}>${type}</option>`
 );
 
-let selectedType1 = '';
-let selectedType2 = '';
+let selectedType1 = "";
+let selectedType2 = "";
 disableSecondTypeSelection();
 
-pokemonDropdown1.addEventListener("change", ()=> {selectedType1 = pokemonDropdown1.value;
-  if (selectedType1 === ""){
-    disableSecondTypeSelection()
+pokemonDropdown1.addEventListener("change", () => {
+  selectedType1 = pokemonDropdown1.value;
+  if (selectedType1 === "") {
+    disableSecondTypeSelection();
   } else {
-  enableSecondTypeSelection();
+    enableSecondTypeSelection();
   }
-})
+});
 
-pokemonDropdown2.addEventListener("change", ()=> {selectedType2= pokemonDropdown2.value;
-    for (let option of pokemonDropdown1.options) {
-      if (option.value === selectedType2) {
-        option.disabled = true;
-      } else {
-        option.disabled = false;
-      }
+pokemonDropdown2.addEventListener("change", () => {
+  selectedType2 = pokemonDropdown2.value;
+  for (let option of pokemonDropdown1.options) {
+    if (option.value === selectedType2) {
+      option.disabled = true;
+    } else {
+      option.disabled = false;
     }
-}
-)
+  }
+});
 
 function enableSecondTypeSelection() {
   pokemonDropdown2.disabled = false;
   for (let option of pokemonDropdown2.options) {
-    if (option.value === selectedType1){
+    if (option.value === selectedType1) {
       option.disabled = true;
     } else {
       option.disabled = false;
@@ -58,14 +60,50 @@ function enableSecondTypeSelection() {
 }
 
 function disableSecondTypeSelection() {
-  pokemonDropdown2.value = '';
+  pokemonDropdown2.value = "";
   pokemonDropdown2.disabled = true;
-
+  for (let option of pokemonDropdown1.options) {
+    option.disabled = false;
+  }
 }
 
-pokemonForm.addEventListener('submit', (e)=>{
+pokemonForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(selectedType1);
-  console.log(selectedType2);
-  
-})
+  determineTypeDamage(selectedType1, selectedType2);
+});
+
+function determineTypeDamage(type1, type2) {
+  let type1Defenses = Object.entries(
+    typeMatchups.filter((type) => type[0] === type1)[0][1]
+  );
+  let type2Defenses = Object.entries(
+    typeMatchups.filter((type) => type[0] === type2)[0][1]
+  );
+  console.log(type1Defenses);
+  console.log(type2Defenses);
+  let damageTotalbyType = [];
+
+  for(let i=0; i< type1Defenses.length ; i++){
+    const [type, multiplier] = type1Defenses[i]    
+    if(type2Defenses.length > 0){
+      damageTotalbyType.push([type, multiplier*type2Defenses[i][1]])
+    } else {
+      damageTotalbyType.push([type, multiplier]);
+    }
+  }
+  console.log(damageTotalbyType);
+
+  const quadrupleDamage = damageTotalbyType.filter((type) => type[1] === 4);
+  const doubleDamage = damageTotalbyType.filter((type) => type[1] === 2);
+  const normalDamage = damageTotalbyType.filter((type) => type[1] === 1);
+  const halfDamage = damageTotalbyType.filter((type) => type[1] === 0.5);
+  const quarterDamage = damageTotalbyType.filter((type) => type[1] === 0.25);
+  const noDamage = damageTotalbyType.filter((type) => type[1] === 0);
+
+  console.log(quadrupleDamage);
+  console.log(doubleDamage);
+  console.log(normalDamage);
+  console.log(halfDamage);
+  console.log(quarterDamage);
+  console.log(noDamage);
+}
